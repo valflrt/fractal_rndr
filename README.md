dce3916fd356698c54827b5ffd0837ba38871d71
valflrt <valflrt@pm.me>
2024-12-01 12:01:06 +0100

    Improvements (see description)
    - Add fractal kind
    - Load fractal parameters from json
    - Change coloring algo (use cumulative histogram)

---
+++ README.md
@@ -0,0 +1,92 @@
+# Fractal Renderer
+
+This is a very simple program used to render fractals using a parameter json file.
+
+It includes different fractal kinds among which Mandelbrot and a (new ?) kind of fractal I came up with by using second/third degree recursive sequence instead of the classic first degree Mandelbrot.
+
+# How to use
+
+Create a json file called `fractal.json` at the root of the directory (next to Cargo.toml).
+
+The file must have the following json structure:
+
+```json
+{
+  "img_width": ..., // make the image as big as you want (not too big tho)
+  "img_height": ...,
+  "zoom": 1., // zoom into the fractal by decreasing this
+  "center_x": 0.0, // change this...
+  "center_y": 0.0, // ... and this to change the render position
+  "fractal_kind": "ThirdOrderGrowingExponent" // this is the fractal kind
+}
+```
+
+Currently available fractal kinds are:
+
+- `Mandelbrot`
+- `SecondOrderGrowingExponent`
+- `ThirdOrderGrowingExponent`
+
+# Examples
+
+![gmawxkdrwbkd](./fractals/gmawxkdrwbkd.png)
+
+```json
+// fractals/gmawxkdrwbkd.json
+{
+  "img_width": 3840,
+  "img_height": 2160,
+  "zoom": 0.00026,
+  "center_x": -0.11591,
+  "center_y": 0.0,
+  "fractal_kind": "ThirdOrderGrowingExponent"
+}
+```
+
+#
+
+![yjtzeggtvbcf](./fractals/yjtzeggtvbcf.png)
+
+```json
+// fractals/yjtzeggtvbcf.json
+{
+  "img_width": 3840,
+  "img_height": 2160,
+  "zoom": 0.001,
+  "center_x": 0.0097,
+  "center_y": 0.01,
+  "fractal_kind": "SecondOrderGrowingExponent"
+}
+```
+
+#
+
+![pafneiqccmnv](./fractals/pafneiqccmnv.png)
+
+```json
+// fractals/pafneiqccmnv.json
+{
+  "img_width": 3840,
+  "img_height": 2160,
+  "zoom": 0.1,
+  "center_x": 0.0,
+  "center_y": 0.0,
+  "fractal_kind": "SecondOrderGrowingExponent"
+}
+```
+
+#
+
+![ftxuudxauwum](./fractals/ftxuudxauwum.png)
+
+```json
+// fractals/ftxuudxauwum.json
+{
+  "img_width": 3840,
+  "img_height": 2160,
+  "zoom": 1e-11,
+  "center_x": -1.99988849682082,
+  "center_y": 1e-16,
+  "fractal_kind": "Mandelbrot"
+}
+```
--- src/main.rs
+++ src/main.rs
@@ -1,85 +1,179 @@
-use std::time::Instant;
+use std::{fs::File, time::Instant};
 
 use image::{ImageBuffer, Rgb};
 use num::complex::Complex;
 use rayon::iter::{ParallelBridge, ParallelIterator};
+use serde::{Deserialize, Serialize};
 
-const IMG_WIDTH: u32 = 1920 * 2;
-const IMG_HEIGHT: u32 = 1080 * 2;
-const ASPECT_RATIO: f64 = IMG_WIDTH as f64 / IMG_HEIGHT as f64;
+const MAX_ITER: u32 = 3000;
 
-const MAX_ITER: u32 = 2000;
-
-// const ZOOM: f64 = 1.;
-// const CENTER_X: f64 = 0.;
-// const CENTER_Y: f64 = 0.;
-
-const ZOOM: f64 = 0.01;
-const CENTER_X: f64 = 0.;
-const CENTER_Y: f64 = 0.045;
-
-const WIDTH: f64 = ZOOM;
-const HEIGHT: f64 = WIDTH / ASPECT_RATIO;
-const X_MIN: f64 = CENTER_X - WIDTH / 2.0;
-const X_MAX: f64 = CENTER_X + WIDTH / 2.0;
-const Y_MIN: f64 = CENTER_Y - HEIGHT / 2.0;
-const Y_MAX: f64 = CENTER_Y + HEIGHT / 2.0;
+#[derive(Debug, Serialize, Deserialize)]
+struct FractalParams {
+    img_width: u32,
+    img_height: u32,
+    zoom: f64,
+    center_x: f64,
+    center_y: f64,
+    fractal_kind: FractalKind,
+}
 
 fn main() {
-    let mut img = ImageBuffer::new(IMG_WIDTH, IMG_HEIGHT);
+    let params = serde_json::from_reader::<_, FractalParams>(
+        File::open("fractal.json").expect("failed to read input param file"),
+    )
+    .expect("failed to decode input param file");
+
+    let aspect_ratio = params.img_width as f64 / params.img_height as f64;
+
+    let width = params.zoom;
+    let height = width / aspect_ratio;
+    let x_min = params.center_x - width / 2.;
+    let x_max = params.center_x + width / 2.;
+    let y_min = params.center_y - height / 2.;
+    let y_max = params.center_y + height / 2.;
+
+    let mut img = ImageBuffer::new(params.img_width, params.img_height);
 
     let start = Instant::now();
 
-    let pixels = img
-        .enumerate_pixels()
+    let pixel_values = (0..params.img_height)
+        .flat_map(|y| (0..params.img_width).map(move |x| (x, y)))
         .par_bridge()
-        .map(|(x, y, _)| {
-            let real = X_MIN + (x as f64 / IMG_WIDTH as f64) * (X_MAX - X_MIN);
-            let imag = Y_MIN + (y as f64 / IMG_HEIGHT as f64) * (Y_MAX - Y_MIN);
+        .map(|(x, y)| {
+            let real = x_min + (x as f64 / params.img_width as f64) * (x_max - x_min);
+            let imag = y_min + (y as f64 / params.img_height as f64) * (y_max - y_min);
             let c = Complex::new(real, imag);
 
-            let iterations = mandelbrot(c, MAX_ITER);
+            let iterations = params.fractal_kind.get_pixel(c);
 
-            (x, y, fancy_color_mapping(iterations, MAX_ITER))
+            (x, y, iterations)
         })
         .collect::<Vec<_>>();
 
-    for (x, y, pixel) in pixels {
-        img.put_pixel(x, y, pixel);
+    let cumulative_histogram = compute_histogram(&pixel_values);
+
+    for (x, y, iterations) in pixel_values {
+        img.put_pixel(
+            x,
+            y,
+            color_mapping(cumulative_histogram[iterations as usize].powi(12)),
+        );
     }
 
     println!("{:?} elapsed", start.elapsed());
 
-    img.save("fractal.png").expect("Failed to save image");
+    img.save("fractal.png").expect("failed to save image");
 }
 
-fn mandelbrot(c: Complex<f64>, max_iter: u32) -> u32 {
-    let mut z0 = Complex::new(0.0, 0.0);
-    let mut z1 = Complex::new(0.0, 0.0);
+#[derive(Debug, Serialize, Deserialize)]
+enum FractalKind {
+    Mandelbrot,
+    SecondOrderGrowingExponent,
+    ThirdOrderGrowingExponent,
+}
 
-    for i in 0..max_iter {
-        if z1.norm_sqr() > 4.0 {
-            return i;
+impl FractalKind {
+    fn get_pixel(&self, c: Complex<f64>) -> u32 {
+        match self {
+            FractalKind::Mandelbrot => {
+                let mut z = Complex::new(0., 0.);
+
+                for i in 0..MAX_ITER {
+                    if z.norm_sqr() > 4. {
+                        return i;
+                    }
+                    z = z * z + c;
+                }
+                MAX_ITER
+            }
+            FractalKind::SecondOrderGrowingExponent => {
+                let mut z0 = Complex::new(0., 0.);
+                let mut z1 = Complex::new(0., 0.);
+
+                for i in 0..MAX_ITER {
+                    if z1.norm_sqr() > 4. {
+                        return i;
+                    }
+                    let new_z1 = z1 * z1 + z0 + c;
+                    z0 = z1;
+                    z1 = new_z1;
+                }
+                MAX_ITER
+            }
+            FractalKind::ThirdOrderGrowingExponent => {
+                let mut z0 = Complex::new(0., 0.);
+                let mut z1 = Complex::new(0., 0.);
+                let mut z2 = Complex::new(0., 0.);
+
+                for i in 0..MAX_ITER {
+                    if z1.norm_sqr() > 4. {
+                        return i;
+                    }
+                    let new_z2 = z2 * z2 * z2 + z1 * z1 + z0 + c;
+                    z0 = z1;
+                    z1 = z2;
+                    z2 = new_z2;
+                }
+                MAX_ITER
+            }
         }
-        let new_z = z1 * z1 + z0 + c;
-        z0 = z1;
-        z1 = new_z;
     }
-    max_iter
 }
 
-fn fancy_color_mapping(iterations: u32, max_iter: u32) -> Rgb<u8> {
-    if iterations == max_iter {
-        Rgb([0, 0, 0])
+fn compute_histogram(pixel_values: &[(u32, u32, u32)]) -> Vec<f64> {
+    let mut histogram = vec![0; MAX_ITER as usize + 1];
+
+    for &(_, _, iteration_count) in pixel_values.iter() {
+        histogram[iteration_count as usize] += 1;
+    }
+
+    let total = histogram.iter().sum::<u32>();
+    let mut cumulative = vec![0.; MAX_ITER as usize + 1];
+    let mut cumulative_sum = 0.;
+    for (i, &count) in histogram.iter().enumerate() {
+        cumulative_sum += count as f64 / total as f64;
+        cumulative[i] = cumulative_sum;
+    }
+
+    cumulative
+}
+const GRADIENT_LENGTH: usize = 8;
+const GRADIENT_VALUES: [f64; GRADIENT_LENGTH] = [0., 0.10, 0.25, 0.4, 0.55, 0.7, 0.85, 0.95];
+const GRADIENT_COLORS: [Rgb<u8>; GRADIENT_LENGTH] = [
+    Rgb([10, 2, 20]),
+    Rgb([200, 40, 230]),
+    Rgb([20, 160, 230]),
+    Rgb([60, 230, 80]),
+    Rgb([255, 230, 20]),
+    Rgb([255, 120, 20]),
+    Rgb([255, 40, 60]),
+    Rgb([2, 0, 4]),
+];
+
+fn color_mapping(t: f64) -> Rgb<u8> {
+    if t <= GRADIENT_VALUES[0] {
+        GRADIENT_COLORS[0]
+    } else if t >= GRADIENT_VALUES[GRADIENT_LENGTH - 1] {
+        GRADIENT_COLORS[GRADIENT_LENGTH - 1]
     } else {
-        let t = iterations as f64 / max_iter as f64;
-
-        let r = (9.0 * (1.0 - t) * t.powi(3) * 255.0).min(255.0).max(0.0) as u8;
-        let g = (15.0 * (1.0 - t).powi(2) * t.powi(2) * 255.0)
-            .min(255.0)
-            .max(0.0) as u8;
-        let b = (8.5 * (1.0 - t).powi(3) * t * 255.0).min(255.0).max(0.0) as u8;
-
-        Rgb([r, g, b])
+        for i in 0..GRADIENT_LENGTH {
+            if GRADIENT_VALUES[i] <= t && t <= GRADIENT_VALUES[i + 1] {
+                let ratio =
+                    (t - GRADIENT_VALUES[i]) / (GRADIENT_VALUES[i + 1] - GRADIENT_VALUES[i]);
+                let Rgb([r1, g1, b1]) = GRADIENT_COLORS[i];
+                let Rgb([r2, g2, b2]) = GRADIENT_COLORS[i + 1];
+                let r = (r1 as f64 * (1. - ratio) + r2 as f64 * ratio)
+                    .min(255.)
+                    .max(0.) as u8;
+                let g = (g1 as f64 * (1. - ratio) + g2 as f64 * ratio)
+                    .min(255.)
+                    .max(0.) as u8;
+                let b = (b1 as f64 * (1. - ratio) + b2 as f64 * ratio)
+                    .min(255.)
+                    .max(0.) as u8;
+                return Rgb([r, g, b]);
+            }
+        }
+        GRADIENT_COLORS[GRADIENT_LENGTH - 1]
     }
 }
