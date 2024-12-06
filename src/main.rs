56ee92ac6dafb29c39f32f48e6ba6ff435ba6579
valflrt <valflrt@pm.me>
2024-12-06 22:17:10 +0100

    Rename fractal kinds and replace for loops with while loops

--- src/main.rs
+++ src/main.rs
@@ -152,9 +152,9 @@ fn main() {
 #[derive(Debug, Serialize, Deserialize)]
 enum FractalKind {
     Mandelbrot,
-    SecondOrderGrowingExponent,
-    ThirdOrderGrowingExponent,
-    NthOrderGrowingExponent(usize),
+    SecondDegreeWithGrowingExponent,
+    ThirdDegreeWithGrowingExponent,
+    NthDegreeWithGrowingExponent(usize),
 }
 
 impl FractalKind {
@@ -164,52 +164,52 @@ impl FractalKind {
             FractalKind::Mandelbrot => {
                 let mut z = Complex::new(0., 0.);
 
-                for i in 0..max_iter {
-                    if z.norm_sqr() > 4. {
-                        return (i, z);
-                    }
+                let mut i = 0;
+                while i < max_iter && z.norm_sqr() < 4. {
                     z = z * z + c;
+                    i += 1;
                 }
-                (max_iter, z)
+
+                (i, z)
             }
-            FractalKind::SecondOrderGrowingExponent => {
+            FractalKind::SecondDegreeWithGrowingExponent => {
                 let mut z0 = Complex::new(0., 0.);
                 let mut z1 = Complex::new(0., 0.);
 
-                for i in 0..max_iter {
-                    if z1.norm_sqr() > 4. {
-                        return (i, z1);
-                    }
+                let mut i = 0;
+                while i < max_iter && z1.norm_sqr() < 4. {
                     let new_z1 = z1 * z1 + z0 + c;
                     z0 = z1;
                     z1 = new_z1;
+
+                    i += 1;
                 }
-                (max_iter, z1)
+
+                (i, z1)
             }
-            FractalKind::ThirdOrderGrowingExponent => {
+            FractalKind::ThirdDegreeWithGrowingExponent => {
                 let mut z0 = Complex::new(0., 0.);
                 let mut z1 = Complex::new(0., 0.);
                 let mut z2 = Complex::new(0., 0.);
 
-                for i in 0..max_iter {
-                    if z1.norm_sqr() > 4. {
-                        return (i, z2);
-                    }
+                let mut i = 0;
+                while i < max_iter && z2.norm_sqr() < 4. {
                     let new_z2 = z2 * z2 * z2 + z1 * z1 + z0 + c;
                     z0 = z1;
                     z1 = z2;
                     z2 = new_z2;
+
+                    i += 1;
                 }
-                (max_iter, z2)
+
+                (i, z2)
             }
-            FractalKind::NthOrderGrowingExponent(n) => {
+            FractalKind::NthDegreeWithGrowingExponent(n) => {
                 let n = *n;
                 let mut z = vec![Complex::new(0., 0.); n];
 
-                for i in 0..max_iter {
-                    if z[n - 1].norm_sqr() > 4. {
-                        return (i, z[n - 1]);
-                    }
+                let mut i = 0;
+                while i < max_iter && z[n - 1].norm_sqr() < 4. {
                     let mut new_z = c;
                     for k in 0..n {
                         new_z += z[k].powi(k as i32 + 1);
@@ -218,8 +218,11 @@ impl FractalKind {
                         z[k] = z[k + 1];
                     }
                     z[n - 1] = new_z;
+
+                    i += 1;
                 }
-                (max_iter, z[n - 1])
+
+                (i, z[n - 1])
             }
         }
     }
