7e5eeb429790a3f5275e30e54d264c762b647b53
valflrt <valflrt@pm.me>
2024-12-07 12:05:09 +0100

    Use rug instead of astro-float (now working but extremely slow)

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,27 @@
-mod calc;
-
 use std::{env, fs::File, time::Instant};
 
-use astro_float::{BigFloat, Consts, RoundingMode};
-use calc::{add, norm_sqr, pow};
 use image::{ImageBuffer, Rgb};
 use rayon::iter::{ParallelBridge, ParallelIterator};
+use rug::{
+    ops::{CompleteRound, Pow},
+    Complex, Float,
+};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
 struct FractalParams {
     img_width: u32,
     img_height: u32,
-    zoom: BigFloat,
-    center_x: BigFloat,
-    center_y: BigFloat,
+    zoom: String,
+    center_x: String,
+    center_y: String,
     max_iter: u32,
     oversampling: Option<bool>,
     fractal_kind: FractalKind,
     coloring_mode: ColoringMode,
 }
 
-/// Floating point precision (1 is 64 bits, 2 is 128 bits, ...)
-const P: usize = 1;
-const RM: RoundingMode = RoundingMode::ToEven;
-
-#[derive(Debug, Clone)]
-struct LocalConsts {
-    one: BigFloat,
-    two: BigFloat,
-    four: BigFloat,
-    half: BigFloat,
-    sin_pi_over_3: BigFloat,
-    zero_c: (BigFloat, BigFloat),
-}
-
-impl Default for LocalConsts {
-    fn default() -> Self {
-        let mut cc = Consts::new().unwrap();
-
-        let zero = BigFloat::from_f32(0., P);
-        let one = BigFloat::from_f32(1., P);
-        let two = BigFloat::from_f32(2., P);
-        let three = BigFloat::from_f32(3., P);
-        let four = BigFloat::from_f32(4., P);
-        let half = BigFloat::from_f32(1., P).div(&two, P, RM);
-        let sin_pi_over_3 = cc.pi(P, RM).div(&three, P, RM).sin(P, RM, &mut cc);
-
-        LocalConsts {
-            one,
-            two,
-            four,
-            half,
-            sin_pi_over_3,
-            zero_c: (zero.clone(), zero),
-        }
-    }
-}
+const P: u32 = 32;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -77,20 +42,18 @@ fn main() {
                     fractal_kind,
                     coloring_mode,
                 }) => {
-                    let consts = LocalConsts::default();
+                    let zoom = Float::parse(zoom).unwrap().complete(P);
+                    let center_x = Float::parse(center_x).unwrap().complete(P);
+                    let center_y = Float::parse(center_y).unwrap().complete(P);
 
-                    let img_width_f = BigFloat::from_f32(img_width as f32, P);
-                    let img_height_f = BigFloat::from_f32(img_height as f32, P);
-
-                    let aspect_ratio = img_width_f.div(&img_height_f, P, RM);
+                    let aspect_ratio = img_width as f64 / img_height as f64;
 
                     let width = zoom;
-                    let height = width.div(&aspect_ratio, P, RM);
-
-                    let x_min = center_x.sub(&width.div(&consts.two, P, RM), P, RM);
-                    let x_max = center_x.add(&width.div(&consts.two, P, RM), P, RM);
-                    let y_min = center_y.sub(&height.div(&consts.two, P, RM), P, RM);
-                    let y_max = center_y.add(&height.div(&consts.two, P, RM), P, RM);
+                    let height = width.clone() / aspect_ratio;
+                    let x_min: Float = &center_x - width.clone() / 2.;
+                    let x_max: Float = &center_x + width / 2.;
+                    let y_min: Float = &center_y - height.clone() / 2.;
+                    let y_max: Float = &center_y + height / 2.;
 
                     let mut img = ImageBuffer::new(img_width, img_height);
 
@@ -100,68 +63,34 @@ fn main() {
                         .flat_map(|y| (0..img_width).map(move |x| (x, y)))
                         .par_bridge()
                         .map(|(x, y)| {
-                            let x_f = BigFloat::from_f32(x as f32, P);
-                            let y_f = BigFloat::from_f32(y as f32, P);
-
-                            let dx = x_max.sub(&x_min, P, RM);
-                            let dy = y_max.sub(&y_min, P, RM);
+                            let dx = x_max.clone() - x_min.clone();
+                            let dy = y_max.clone() - y_min.clone();
 
                             if let Some(true) = oversampling {
-                                let re1 = x_min.add(
-                                    &x_f.add(&consts.one, P, RM)
-                                        .div(&img_width_f, P, RM)
-                                        .mul(&dx, P, RM),
-                                    P,
-                                    RM,
-                                );
-                                let im1 = y_min.add(
-                                    &y_f.div(&img_height_f, P, RM).mul(&dy, P, RM),
-                                    P,
-                                    RM,
-                                );
-                                let c1 = (re1, im1);
+                                let real1 = &x_min + (x as f64 / img_width as f64) * dx.clone();
+                                let imag1 = &y_min + (y as f64 / img_height as f64) * dy.clone();
+                                let c1 = Complex::with_val(P, (real1, imag1));
 
-                                let re2 = x_min.add(
-                                    &x_f.sub(&consts.half, P, RM)
-                                        .div(&img_width_f, P, RM)
-                                        .mul(&dx, P, RM),
-                                    P,
-                                    RM,
-                                );
-                                let im2 = y_min.add(
-                                    &y_f.add(&consts.sin_pi_over_3, P, RM)
-                                        .div(&img_height_f, P, RM)
-                                        .mul(&dy, P, RM),
-                                    P,
-                                    RM,
-                                );
-                                let c2 = (re2.clone(), im2);
+                                let real2 = &x_min + ((x as f64 - 0.5) / img_width as f64) * dx;
+                                let imag2 = &y_min
+                                    + ((y as f64 + 0.866025) / img_height as f64) * dy.clone();
+                                let c2 = Complex::with_val(P, (real2.clone(), imag2));
 
-                                let im3 = y_min.add(
-                                    &y_f.sub(&consts.sin_pi_over_3, P, RM)
-                                        .div(&img_height_f, P, RM)
-                                        .mul(&dy, P, RM),
-                                    P,
-                                    RM,
-                                );
-                                let c3 = (re2, im3);
+                                let imag3 =
+                                    &y_min + ((y as f64 - 0.866025) / img_height as f64) * dy;
+                                let c3 = Complex::with_val(P, (real2, imag3));
 
-                                let (iter1, _) = fractal_kind.get_pixel(c1, max_iter, &consts);
-                                let (iter2, _) = fractal_kind.get_pixel(c2, max_iter, &consts);
-                                let (iter3, _) = fractal_kind.get_pixel(c3, max_iter, &consts);
+                                let (iter1, _) = fractal_kind.get_pixel(c1, max_iter);
+                                let (iter2, _) = fractal_kind.get_pixel(c2, max_iter);
+                                let (iter3, _) = fractal_kind.get_pixel(c3, max_iter);
 
                                 (x, y, ((iter1 + iter2 + iter3) as f64 / 3.) as u32)
                             } else {
-                                let re =
-                                    x_min.add(&x_f.div(&img_width_f, P, RM).mul(&dx, P, RM), P, RM);
-                                let im = y_min.add(
-                                    &y_f.div(&img_height_f, P, RM).mul(&dy, P, RM),
-                                    P,
-                                    RM,
-                                );
-                                let c = (re, im);
+                                let real = &x_min + (x as f64 / img_width as f64) * dx;
+                                let imag = &y_min + (y as f64 / img_height as f64) * dy;
+                                let c = Complex::with_val(P, (real, imag));
 
-                                let (iterations, _) = fractal_kind.get_pixel(c, max_iter, &consts);
+                                let (iterations, _) = fractal_kind.get_pixel(c, max_iter);
 
                                 (x, y, iterations)
                             }
@@ -217,7 +146,6 @@ fn main() {
                     println!("{:?} elapsed", start.elapsed());
 
                     img.save(&args[2]).expect("failed to save fractal image");
-                    // fs::write("out.json", serde_json::to_string_pretty(&params).unwrap()).unwrap();
                 }
                 Err(err) => {
                     println!("error reading parameter file: {}", err);
@@ -230,48 +158,35 @@ fn main() {
     }
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone, Copy, Serialize, Deserialize)]
 enum FractalKind {
     Mandelbrot,
     SecondDegreeWithGrowingExponent,
     ThirdDegreeWithGrowingExponent,
-    // NthDegreeWithGrowingExponent(usize),
 }
 
 impl FractalKind {
     /// Outputs (iteration_count, escape_z)
-    fn get_pixel(
-        &self,
-        c: (BigFloat, BigFloat),
-        max_iter: u32,
-        consts: &LocalConsts,
-    ) -> (u32, (BigFloat, BigFloat)) {
+    fn get_pixel(&self, c: Complex, max_iter: u32) -> (u32, Complex) {
         match self {
             FractalKind::Mandelbrot => {
-                let mut z = consts.zero_c.clone();
+                let mut z = Complex::with_val(P, (0., 0.));
 
                 let mut i = 0;
-                while i < max_iter {
-                    if norm_sqr(&z) > consts.four {
-                        break;
-                    }
-                    z = add(&pow(&z, 2), &c);
-
+                while i < max_iter && z.clone().abs().real() < &4 {
+                    z = z.pow(2) + &c;
                     i += 1;
                 }
 
                 (i, z)
             }
             FractalKind::SecondDegreeWithGrowingExponent => {
-                let mut z0 = consts.zero_c.clone();
-                let mut z1 = consts.zero_c.clone();
+                let mut z0 = Complex::with_val(P, (0., 0.));
+                let mut z1 = Complex::with_val(P, (0., 0.));
 
                 let mut i = 0;
-                while i < max_iter {
-                    if norm_sqr(&z1) > consts.four {
-                        break;
-                    }
-                    let new_z1 = add(&add(&pow(&z1, 2), &z0), &c);
+                while i < max_iter && z1.clone().abs().real() < &4 {
+                    let new_z1 = z1.clone().pow(2) + &z0 + &c;
                     z0 = z1;
                     z1 = new_z1;
 
@@ -281,16 +196,13 @@ impl FractalKind {
                 (i, z1)
             }
             FractalKind::ThirdDegreeWithGrowingExponent => {
-                let mut z0 = consts.zero_c.clone();
-                let mut z1 = consts.zero_c.clone();
-                let mut z2 = consts.zero_c.clone();
+                let mut z0 = Complex::with_val(P, (0., 0.));
+                let mut z1 = Complex::with_val(P, (0., 0.));
+                let mut z2 = Complex::with_val(P, (0., 0.));
 
                 let mut i = 0;
-                while i < max_iter {
-                    if norm_sqr(&z2) > consts.four {
-                        break;
-                    }
-                    let new_z2 = add(&add(&add(&pow(&z2, 3), &pow(&z1, 2)), &z0), &c);
+                while i < max_iter && z2.clone().abs().real() < &4 {
+                    let new_z2 = z2.clone().pow(3) + z1.clone().pow(2) + &z0 + &c;
                     z0 = z1;
                     z1 = z2;
                     z2 = new_z2;
@@ -299,34 +211,12 @@ impl FractalKind {
                 }
 
                 (i, z2)
-            } // FractalKind::NthDegreeWithGrowingExponent(n) => {
-              //     let n = *n;
-              //     let mut z = vec![Complex::new(0., 0.); n];
-
-              //     let mut i = 0;
-              //     while i < max_iter {
-              //         if z[n - 1].norm_sqr() > 4.into() {
-              //             break;
-              //         }
-              //         let mut new_z = c;
-              //         for k in 0..n {
-              //             new_z += z[k].powu(k as u32 + 1);
-              //         }
-              //         for k in 0..n - 1 {
-              //             z[k] = z[k + 1];
-              //         }
-              //         z[n - 1] = new_z;
-
-              //         i += 1;
-              //     }
-
-              //     (i, z[n - 1])
-              // }
+            }
         }
     }
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone, Copy, Serialize, Deserialize)]
 enum ColoringMode {
     BlackAndWhite,
     Linear,
