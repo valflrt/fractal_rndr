b9fcbd14381f0c6d08b9ae5aed18ad2b4abb0f99
valflrt <valflrt@pm.me>
2026-02-17 18:36:32 +0100

    Add and remove some presets

--- src/presets.rs
+++ src/presets.rs
@@ -1,8 +1,10 @@
 pub const PRESETS: &[(&str, &str)] = &[
     ("cyggmf", include_str!("../presets/cyggmf.ron")),
+    ("gkiqsf", include_str!("../presets/gkiqsf.ron")),
+    ("icbets", include_str!("../presets/icbets.ron")),
+    ("iskihubju", include_str!("../presets/iskihubju.ron")),
     ("kajan-unmyai", include_str!("../presets/kajan-unmyai.ron")),
+    ("naikomsai", include_str!("../presets/naikomsai.ron")),
     ("sehokynka", include_str!("../presets/sehokynka.ron")),
-    ("vifxif", include_str!("../presets/vifxif.ron")),
-    ("utxwso", include_str!("../presets/utxwso.ron")),
-    ("yvajbc", include_str!("../presets/yvajbc.ron")),
+    ("su-nunai", include_str!("../presets/su-nunai.ron")),
 ];
